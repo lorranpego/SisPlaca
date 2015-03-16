b8dcebe3acd8b01adaae4b6ee681eba5b4998c05
lorran <lorran@lohan-air>
2015-03-16 18:45:47 -0300

    Criado modelo.

---
+++ SisPlaca/src/model/carro.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.awt.Image;
+
+/**
+ *
+ * @author lorran
+ */
+public class carro {
+    
+    private int id;
+    private String placa;
+    private String marca;
+    private String modelo;
+    private String cor;
+    private int ativo;
+    private Image foto; //Checar se tipo deve ser Image
+
+    
+    
+    public carro(String placa, String marca, String modelo, String cor, int ativo, Image foto) {
+        this.placa = placa;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.cor = cor;
+        this.ativo = ativo;
+        this.foto = foto;
+    }
+
+    public carro(int id, String placa, String marca, String modelo, String cor, int ativo, Image foto) {
+        this.id = id;
+        this.placa = placa;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.cor = cor;
+        this.ativo = ativo;
+        this.foto = foto;
+    }
+
+    public carro() {
+    }
+    
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(int ativo) {
+        this.ativo = ativo;
+    }
+
+    public Image getFoto() {
+        return foto;
+    }
+
+    public void setFoto(Image foto) {
+        this.foto = foto;
+    }
+    
+    
+    
+}
