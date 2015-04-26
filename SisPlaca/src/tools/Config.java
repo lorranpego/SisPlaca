0c90f6aa0ee085dd22324e1d6d0a0220ddbe8438
lorran <lorran@192.168.25.184>
2015-04-26 09:00:46 -0300

    Modificado configuracoes de banco

--- SisPlaca/src/tools/Config.java
+++ SisPlaca/src/tools/Config.java
@@ -13,7 +13,12 @@ public class Config {
     //private static Properties props = new Properties();
     
     public static final String NOME_DRIVER = "com.mysql.jdbc.Driver";
-    public static final String BD_URL = "jdbc:mysql://173.244.187.177:3306/testes";
-    public static final String BD_LOGIN = "testes";
-    public static final String BD_SENHA = "94peRhtj8Qt178jQ";  
+    
+    public static final String BD_URL = "jdbc:mysql://localhost:3306/sisplaca";
+    public static final String BD_LOGIN = "root";
+    public static final String BD_SENHA = "123456";  
+    
+//    public static final String BD_URL = "jdbc:mysql://173.244.187.177:3306/testes";
+//    public static final String BD_LOGIN = "testes";
+//    public static final String BD_SENHA = "94peRhtj8Qt178jQ";  
 }
