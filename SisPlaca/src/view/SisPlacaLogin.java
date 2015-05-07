2dd98154adb88964df307a13da7aceda0b483fcb
RaphaelGNunes <ranunesx@gmail.com>
2015-05-07 17:30:57 -0300

    Tela de editar carro criada

--- SisPlaca/src/view/SisPlacaLogin.java
+++ SisPlaca/src/view/SisPlacaLogin.java
@@ -63,6 +63,8 @@ public class SisPlacaLogin extends javax.swing.JFrame {
         jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel2.setText("SENHA:");
 
+        login.setText("admin");
+
         BTEntrar.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         BTEntrar.setText("ENTRAR");
         BTEntrar.addActionListener(new java.awt.event.ActionListener() {
@@ -70,6 +72,8 @@ public class SisPlacaLogin extends javax.swing.JFrame {
                 BTEntrarActionPerformed(evt);
             }
         });
+
+        senha.setText("admin");
 
         mensagem.setVisible(false);
 
